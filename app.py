068c9fcf96d338be6cb182d8f7bce4a2a798d951
Payton Miller <payton.miller404@gmail.com>
2020-04-24 21:26:56 -0300

    Website initialization

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("templates/index,html")
+    return render_template("/index.html")
+@app.route("/login")
+def login():
+    return render_template("/login.html")
+@app.route('/api/login', methods=["POST"])
+def actionLogin():
+    data = request.form
+    username = data["username"]
+    password = data["password"]
