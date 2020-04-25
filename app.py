7466cc8092563b442207281676ef928ed90d6d7a
BlackCoffee77 <zbattleman@gmail.com>
2020-04-25 18:40:38 -0400

    fix layout of feed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import *
 import pyrebase
 import ast 
 from getpass import getpass
@@ -19,93 +19,100 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/index.html")
+	uids = [i.val()["uid"] for i in db.child("users").get().each()]
+	if request.cookies.get('uid') in uids:
+		print(1)
+		return render_template("/landingpage.html")
+	else:
+		print(0)
+		return render_template("/index.html")
+	return ""
 
 @app.route("/register")
 def register():
-    return render_template("/register.html")
+	return render_template("/register.html")
 
 @app.route('/api/register', methods=["POST"])
 def actionRegister():
-    data = request.form
-    email = data["email"]
-    password = data["password"]
-    firstName = data["firstName"]
-    lastName = data["lastName"]
-    bio = data["bio"]
-    skills= data["skills"]
-    print(skills[:-1])
-    skills = skills[:-1].split(",")
-    print(skills)
-    createAccount(db, auth, email, password, firstName, lastName, bio, skills)
-    return "penis"
+	data = request.form
+	email = data["email"]
+	password = data["password"]
+	firstName = data["firstName"]
+	lastName = data["lastName"]
+	bio = data["bio"]
+	skills= data["skills"]
+	print(skills[:-1])
+	skills = skills[:-1].split(",")
+	print(skills)
+	createAccount(db, auth, email, password, firstName, lastName, bio, skills)
+	return "penis"
 
 @app.route("/login")
 def login():
-    return render_template("/login.html")
+	return render_template("/login.html")
 
 def setCookie(key, value):
-    resp = make_response("Setting cookie")
-    resp.set_cookie(key, value)
-    return resp
+	resp = make_response("Setting cookie")
+	resp.set_cookie(key, value)
+	return resp
 
 @app.route("/api/login", methods=["POST"])
 def actionLogin():
-    data = request.form
-    email = data["email"]
-    password = data["password"]
-    uid = loginAccount(db, auth, email, password)
+	data = request.form
+	email = data["email"]
+	password = data["password"]
+	uid = loginAccount(db, auth, email, password)
 
-    resp = make_response(render_template("index.html"))
-    resp.set_cookie("uid", uid)
-    return resp
+	resp = make_response(redirect("/"))
+	resp.set_cookie("uid", uid)
+	return resp
 
 @app.route("/api/createPost", methods=["POST"])
 def actionPost():
-    data = request.form
-    uid = request.cookies.get("uid")
-    desc = data["description"]
-    img = data["image"]
-    loc = data["location"]
-    ts = int(time.time())
-    return createPost(db, uid, desc, img, loc, ts)
+	data = request.form
+	uid = request.cookies.get("uid")
+	desc = data["description"]
+	img = data["image"]
+	loc = data["location"]
+	ts = int(time.time())
+	return createPost(db, uid, desc, img, loc, ts)
 
 @app.route("/api/deletePost", methods=["POST"])
 def actionDelete():
-    data = request.form
-    uid = request.cookies.get("uid")
-    postid = data["postid"]
-    if(not deletePost(db, uid, postid)):
-        return "fail"
-    return "success"
+	data = request.form
+	uid = request.cookies.get("uid")
+	postid = data["postid"]
+	if(not deletePost(db, uid, postid)):
+		return "fail"
+	return "success"
 
 @app.route("/api/likePost", methods=["POST"])
 def actionLike():
-    data = request.form
-    uid = request.cookies.get("uid")
-    postid = data["postid"]
-    if(not likePost(db, uid, postid)):
-        return "fail"
-    return "success"
+	data = request.form
+	uid = request.cookies.get("uid")
+	postid = data["postid"]
+	if(not likePost(db, uid, postid)):
+		return "fail"
+	return "success"
 
 @app.route("/api/attendPost", methods=["POST"])
 def actionAttend():
-    data = request.form
-    uid = request.cookies.get("uid")
-    postid = data["postid"]
-    if(not confirmAttendance(db, uid, postid)):
-        return "fail"
-    return "success"
+	data = request.form
+	uid = request.cookies.get("uid")
+	postid = data["postid"]
+	if(not confirmAttendance(db, uid, postid)):
+		return "fail"
+	return "success"
 
 @app.route("/api/followingPosts", methods=["GET"])
 def getFollowingPosts():
-    data = request.form
-    uid = request.cookies.get("uid")
-    ans = fetchFollowingPost(db, uid)
-    return json.dumps(ans)
+	data = request.form
+	uid = request.cookies.get("uid")
+	ans = fetchFollowingPost(db, uid)
+	return json.dumps(ans)
 
 @app.route("/get")
 def getcookie():
-    return ""
+	return ""
 
 app.run()
